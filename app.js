078f3f3343a1fbdd923236dd1342622c091365aa
tfxl <CAB022105@coderacademy.edu.au>
2022-01-31 17:10:24 +1000

    Connection to database, using Pool, with variables in .env file. .get request a SELECT query demonstrates connection established and working

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const cors = require('cors')
 
 require('dotenv').config()
 
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  host: process.env.HOST,
+  port: process.env.PORT,
+  database: process.env.DATABASENAME
+})
+
 const app = express()
 app.use(cors())           // cors middleware to assist around the CORS issue during development
 app.use(express.json())   // this allows the req.body to access as json object
@@ -13,5 +22,21 @@ app.get('/', (request, response) => {
   response.send( { myResponse : "Confirmation that .get request was processed " })
 })
 
+app.get('/seeds', (req, res) => {
+
+  let query_string = 
+  `SELECT type, variety 
+    FROM seed`
+    
+  pool.query( query_string, (error, results) => {
+    if (error) {
+      res.status(422).send({ error: error.message })
+    } else if (results.rows.length == 0) {
+        res.status(404).send({ error: 'No entries' })
+    } else {
+        res.send(results.rows)
+    }
+  })
+})
 
 module.exports = app
