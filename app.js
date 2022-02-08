fec153661a3238272abd45c87159096ad8faf508
tfxl <CAB022105@coderacademy.edu.au>
2022-02-08 20:45:26 +1000

    New endpoint for moving location of pallet

--- app.js
+++ app.js
@@ -291,7 +291,7 @@ app.delete('/warehouse/:warehouse_id/lot/:lot_code', (req, res) => {
 })
 
 
-////////////////////////////////////// UPDATE PRODUCT, LOT AND LOCATION ///////////////////////////////////////////
+////////////////////////////////////// UPDATE PRODUCT, LOT, LOCATION ///////////////////////////////////////////
 
 // UPDATE PRODUCT
 
@@ -371,7 +371,7 @@ app.put('/warehouse/:warehouse_id/lot/:lot_code', (req, res) => {
   })
 })
 
-// UPDATE LOCATION
+// UPDATE LOCATION TYPE
 
 app.put('/warehouse/:warehouse_id/locations', (req, res) => {
 
@@ -409,7 +409,38 @@ app.put('/warehouse/:warehouse_id/locations', (req, res) => {
   })
 })
 
+// UPDATE SPECIFIC PALLET LOCATION
 
+app.put('/pallet/:pallet_id/location/:new_location', (req, res) => {
+
+  let pallet_id = req.params.pallet_id
+  let new_location_coords = req.params.new_location
+
+  let query_string = 
+  `
+  UPDATE pallet
+    SET location_id = (
+      SELECT location.id 
+        FROM location
+          WHERE location.warehouse_id = (
+            SELECT warehouse.id
+                  FROM warehouse
+                INNER JOIN location ON warehouse.id = location.warehouse_id
+                        INNER JOIN pallet ON pallet.id = location.id	
+              WHERE pallet.id = $1
+          ) AND location.coord = $2
+      ) WHERE pallet.id = $1
+  `
+  pool.query(query_string, [ pallet_id, new_location_coords ], 
+    (error, _) => {
+
+      if (error) {
+          res.status(422).send({ error: error.message })
+      } else {
+          res.send(`pallet #${pallet_id} moved to location ${new_location_coords}`)
+      }
+    })
+})
 
 ////////////////////////////  CREATE PRODUCT, LOT, WAREHOUSE  ///////////////////////////////////////////
 
